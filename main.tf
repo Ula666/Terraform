fb13a2c64b1f39756c4afd0635a5586405d70d3c
rrose666 <uziolko@hotmail.com>
2021-05-11 15:22:38 +0100

    added new file

--- main.tf
+++ main.tf
@@ -19,7 +19,8 @@ provider "aws"{
 
 resource "aws_instance" "app_instance"{
 	# add the AMI id between "" as below
-	ami = "ami-0fef271f53d5912fe"
+	# var.name_of_the_resource
+	ami = var.webapp_ami_id 
 
 	# Let's add the type of instance we would like to launch
 	instance_type = "t2.micro"
@@ -32,22 +33,36 @@ resource "aws_instance" "app_instance"{
 
 	# Tags is to give name to our instnace
 	tags = {
-		Name = "eng84_ula_terraform_node_app"
+		Name = "${var.name}"
 	}
 }
+#key_name = "eng84devops"
+#public_key = var.key
 # Resource block of code ends here 
 
 
 # block of code to create a default VPC
-resource "aws_default_vpc" "Terraform_vpc_code_test"{
+#resource "aws_default_vpc" "Terraform_vpc_code_test"{
 	#cidr_block = "10.0.0.0/16"
 	#instance_tenancy = "default"
 
 
-	tags = {
-		Name = "eng84ula_terraform_vpc"
-	}
+	#tags = {
+	#	Name = "eng84ula_terraform_vpc"
+	#}
+#}
+
+# block of code to create a VPC
+resource "aws_vpc" "ula_terraform_vpc" {
+  cidr_block = "30.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "eng84_ula_terraform_vpc"
+  }
 }
+
+
 
 
 # block of code to create a default VPC
@@ -62,7 +77,47 @@ resource "aws_default_vpc" "Terraform_vpc_code_test"{
 
 # Resource block of code for VPC ends here 
 
+# subnet
+resource "aws_subnet" "testing_subnet_ula" {
+	#vpc_id = var.vpc_id
+  vpc_id = aws_vpc.ula_terraform_vpc.id
+	cidr_block = "30.0.3.0/24"
+	availability_zone = "eu-west-1a"
+	tags = {
+		Name = "Ula_Terraform_Subnet"
+	}
+}
 
+
+
+
+#security group
+resource "aws_security_group" "ula_terraform_code_test_sg" {
+	name = "ula_terrafomr_code_test"
+	description = "app group"
+	#vpc_id = var.vpc_id
+  vpc_id = aws_vpc.ula_terraform_vpc.id
+
+
+
+# inbound rules for our app
+	ingress {
+		from_port = "80"
+		to_port = "80"
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"] #a llow all
+	}
+
+# outbound rules
+
+	egress {
+		from_port = 0
+		to_port = 0
+		protocol = "-1"
+		cidr_blocks = ["0.0.0.0/0"] #a llow all
+	}
+
+}
 
 # terraform init
 # terraform plan
---
+++ variable.tf
@@ -0,0 +1,27 @@
+# new file
+
+variable "vpc_id" {
+	default = "vpc-07e47e9d90d2076da"
+}
+
+variable "name" {
+	default = "ula_terraform_variable_code_testing"
+}
+
+variable "webapp_ami_id" {
+	default = "ami-0fef271f53d5912fe"
+}
+
+variable "aws_subnet" {
+	default = "terraform_code_testing_with_subnet_var_ula"
+}
+
+variable "aws_key_name" {
+	default = "eng84devops"
+}
+
+variable "aws_key_path" {
+	default = "~/.ssh/eng84devops.pem"
+}
+
+
