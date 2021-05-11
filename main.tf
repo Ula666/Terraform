8a377d8b8f2b0d53665d4cef2b7f181f37ef3245
rrose666 <uziolko@hotmail.com>
2021-05-11 16:34:32 +0100

    updated main file and variable file

--- main.tf
+++ main.tf
@@ -12,30 +12,107 @@ provider "aws"{
 	region = "eu-west-1"
 } 
 
+# block of code to create a VPC
+resource "aws_vpc" "ula_terraform_vpc" {
+  cidr_block = "30.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "eng84_ula_terraform_vpc"
+  }
+}
+# block of code to create a default VPC
+
+
+# create internet gateway
+resource "aws_internet_gateway" "terraform_ula_igw" {
+  vpc_id = aws_vpc.ula_terraform_vpc.id
+
+  tags = {
+    Name = var.igw_name
+  }
+}
+
+# create main route table
+resource "aws_default_route_table" "terraform_ula_public_rt" {
+  default_route_table_id = aws_vpc.ula_terraform_vpc.default_route_table_id
+
+    route {
+      # associated subnet can reach everywhere
+      cidr_block = "0.0.0.0/0"
+      # ig to reach internet
+      gateway_id = aws_internet_gateway.terraform_ula_igw.id
+    } 
+
+    tags = {
+      Name = var.pub_rt_name
+    } 
+}
+
+
+# create public subnet
+resource "aws_subnet" "terraform_public_subnet" {
+  #vpc_id = var.vpc_id
+  vpc_id = aws_vpc.ula_terraform_vpc.id
+  cidr_block = "30.0.3.0/24"
+  tags = {
+    Name = "Ula_Terraform_Subnet"
+  }
+}
+
+# associate route table with public subnet
+resource "aws_route_table_association" "rt_public_subnet_1" {
+  subnet_id = aws_subnet.terraform_public_subnet.id
+  route_table_id = aws_vpc.ula_terraform_vpc.default_route_table_id
+}
+
+#security group
+resource "aws_security_group" "public_sec_group" {
+  name = var.pub_sec_name
+  description = "app security group"
+  #vpc_id = var.vpc_id
+  vpc_id = aws_vpc.ula_terraform_vpc.id
+
+# inbound rules for our app
+  ingress {
+    from_port = "80"
+    to_port = "80"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] #a llow all
+  }
+
+# outbound rules
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"] #a llow all
+  }
+
+}
+
+
 # Launching an EC2 instance from our Node_app AMI
 # resource is the keyword that allows us to add aws resource 
 # Resource block of code:
-
-
 resource "aws_instance" "app_instance"{
-	# add the AMI id between "" as below
-	# var.name_of_the_resource
-	ami = var.webapp_ami_id 
+  # add the AMI id between "" as below
+  # var.name_of_the_resource
+  ami = var.webapp_ami_id 
+  # Let's add the type of instance we would like to launch
+  instance_type = "t2.micro"
+  # we need to enable public ip for our app
+  associate_public_ip_address = true
+  key_name = var.aws_key_name
+  subnet_id = aws_subnet.terraform_public_subnet.id
+  security_groups = [aws_security_group.public_sec_group.id]
 
-	# Let's add the type of instance we would like to launch
-	instance_type = "t2.micro"
-
-	#AWS_ACCESS_KEY = "AWS_ACCESS_KEY_ID"
-	#AWS_ACCESS_SECRET = "AWS_ACCESS_SECRET"
-
-	# we need to enable public ip for our app
-	associate_public_ip_address = true
-
-	# Tags is to give name to our instnace
-	tags = {
-		Name = "${var.name}"
-	}
+  # Tags is to give name to our instnace
+  tags = {
+    Name = "${var.name}"
+  }
 }
+
 #key_name = "eng84devops"
 #public_key = var.key
 # Resource block of code ends here 
@@ -52,20 +129,7 @@ resource "aws_instance" "app_instance"{
 	#}
 #}
 
-# block of code to create a VPC
-resource "aws_vpc" "ula_terraform_vpc" {
-  cidr_block = "30.0.0.0/16"
-  instance_tenancy = "default"
 
-  tags = {
-    Name = "eng84_ula_terraform_vpc"
-  }
-}
-
-
-
-
-# block of code to create a default VPC
 #resource "aws_default_vpc" "default" {
 	#cidr_block = "10.0.0.0/16"
 	#instance_tenancy = "default"
@@ -77,47 +141,6 @@ resource "aws_vpc" "ula_terraform_vpc" {
 
 # Resource block of code for VPC ends here 
 
-# subnet
-resource "aws_subnet" "testing_subnet_ula" {
-	#vpc_id = var.vpc_id
-  vpc_id = aws_vpc.ula_terraform_vpc.id
-	cidr_block = "30.0.3.0/24"
-	availability_zone = "eu-west-1a"
-	tags = {
-		Name = "Ula_Terraform_Subnet"
-	}
-}
-
-
-
-
-#security group
-resource "aws_security_group" "ula_terraform_code_test_sg" {
-	name = "ula_terrafomr_code_test"
-	description = "app group"
-	#vpc_id = var.vpc_id
-  vpc_id = aws_vpc.ula_terraform_vpc.id
-
-
-
-# inbound rules for our app
-	ingress {
-		from_port = "80"
-		to_port = "80"
-		protocol = "tcp"
-		cidr_blocks = ["0.0.0.0/0"] #a llow all
-	}
-
-# outbound rules
-
-	egress {
-		from_port = 0
-		to_port = 0
-		protocol = "-1"
-		cidr_blocks = ["0.0.0.0/0"] #a llow all
-	}
-
-}
 
 # terraform init
 # terraform plan
--- variable.tf
+++ variable.tf
@@ -1,20 +1,40 @@
 # new file
-
-variable "vpc_id" {
-	default = "vpc-07e47e9d90d2076da"
+variable "vpc_name" {
+  default = "ula_terraform_vpc"
 }
 
+variable "pub_subnet_name" {
+  default = "testing_subnet_ula"
+}
+
+variable "igw_name"{
+  default = "terraform_ula_igw"
+}
+
+variable "pub_rt_name"{
+  default = "terraform_ula_main_rt"
+}
+
+variable "pub_sec_name"{
+  default = "eng84_ula_pub_sec"
+}
+
+
+
+
+
+
 variable "name" {
-	default = "ula_terraform_variable_code_testing"
+	default = "eng84_ula_terraform_app"
 }
 
 variable "webapp_ami_id" {
 	default = "ami-0fef271f53d5912fe"
 }
 
-variable "aws_subnet" {
-	default = "terraform_code_testing_with_subnet_var_ula"
-}
+# variable "aws_subnet" {
+# 	default = "terraform_code_testing_with_subnet_var_ula"
+# }
 
 variable "aws_key_name" {
 	default = "eng84devops"
